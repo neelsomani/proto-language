ff17d90a4ae2c6fefddd60e8ed755329042f7f81
Brian Hie <blhie1@gmail.com>
2025-05-04 17:11:12 -0700

    Refactor tools, add structure pred
    Co-authored-by: Daniel Guo <dguo8412@gmail.com>

--- language/utils.py
+++ language/utils.py
@@ -1,14 +1,8 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from io import StringIO
-from typing import Union, List
-
-import numpy as np
 from biotite.structure import AtomArray
 from biotite.structure.io.pdb import PDBFile
+from io import StringIO
+import numpy as np
+from typing import Union, List
 
 
 def pdb_file_to_atomarray(pdb_path: Union[str, StringIO]) -> AtomArray:
@@ -17,34 +11,3 @@ def pdb_file_to_atomarray(pdb_path: Union[str, StringIO]) -> AtomArray:
 
 def get_atomarray_in_residue_range(atoms: AtomArray, start: int, end: int) -> AtomArray:
     return atoms[np.logical_and(atoms.res_id >= start, atoms.res_id < end)]
-
-
-def read_fasta_file(fasta_path: str) -> List[str]:
-    with open(fasta_path, 'r') as file:
-        return [line.strip() for line in file if not line.startswith('>')]
-
-def load_fasta_sequences_phix(fasta_file):
-    """
-    Simple function to load FASTA sequences - removes +~ and truncates at non-ACGT.
-    """
-    sequences = {}
-    current_id = None
-    
-    with open(fasta_file, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-                
-            if line.startswith('>'):
-                current_id = line[1:].strip()
-                sequences[current_id] = ""
-            else:
-                line = line[2:].upper()
-                truncation_index = len(line)
-                for i, char in enumerate(line):
-                    if char not in 'ACGT':
-                        truncation_index = i
-                        break
-                sequences[current_id] = line[:truncation_index]
-    return sequences
